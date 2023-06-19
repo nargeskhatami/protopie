41319708c94dfb278e531a4652f398afe622207b
Narges Khatami <khatami@citynet.ir>
2023-06-19 17:49:38 +0330

    Change block button spacing styles and add clean props

--- components/Common/Button/ButtonBlock.tsx
+++ components/Common/Button/ButtonBlock.tsx
@@ -4,14 +4,18 @@ const useStyles = makeStyles({
   block: {
     width: "100%",
     height: "56px",
+    "> span": {
+      marginLeft: "var(--spacingHorizontalM)",
+      marginRight: "var(--spacingHorizontalM)",
+    },
   },
 });
 
-export default function ButtonFullWidth(
-  props: React_2.ForwardRefExoticComponent<
-    ButtonProps & React_2.RefAttributes<HTMLButtonElement | HTMLAnchorElement>
-  > & { children?: React.ReactNode }
-) {
+type Props = React_2.ForwardRefExoticComponent<
+  ButtonProps & React_2.RefAttributes<HTMLButtonElement | HTMLAnchorElement>
+> & { children?: React.ReactNode };
+
+export default function ButtonFullWidth(props: Props) {
   const styles = useStyles();
   return (
     <Button {...props} className={`${props.className} ${styles.block}`}>
