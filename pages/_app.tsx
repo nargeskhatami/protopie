dfef45b26255aefb3c78caca3e42120cefbae2ea
Narges Khatami <nargeskhatami.h@gmail.com>
2023-09-07 17:52:13 +0330

    Add ssr support of fluent ui & loading when routes change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 import {
   FluentProvider,
   webDarkTheme,
   Theme,
+  createDOMRenderer,
+  GriffelRenderer,
+  SSRProvider,
+  RendererProvider,
+  webLightTheme,
 } from "@fluentui/react-components";
 import theme from "@/config/theme.json";
 import Head from "next/head";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { PagesProgressBar as ProgressBar } from "next-nprogress-bar";
+import type { AppProps } from "next/app";
 
 export const protopieTheme: Theme = {
   ...webDarkTheme,
@@ -16,17 +22,33 @@ export const protopieTheme: Theme = {
 
 const queryClient = new QueryClient();
 
-export default function App({ Component, pageProps }: AppProps) {
+type EnhancedAppProps = AppProps & { renderer?: GriffelRenderer };
+
+export default function App({
+  Component,
+  pageProps,
+  renderer,
+}: EnhancedAppProps) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <FluentProvider dir="rtl" theme={protopieTheme}>
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </FluentProvider>
+      <RendererProvider renderer={renderer || createDOMRenderer()}>
+        <SSRProvider>
+          <FluentProvider dir="rtl" theme={protopieTheme}>
+            <QueryClientProvider client={queryClient}>
+              <Component {...pageProps} />
+              <ProgressBar
+                height="4px"
+                color="#7d6fe2"
+                options={{ showSpinner: false }}
+                shallowRouting
+              />
+            </QueryClientProvider>
+          </FluentProvider>
+        </SSRProvider>
+      </RendererProvider>
     </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,90 @@
-import Document, { DocumentContext } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+// import Document, { DocumentContext } from "next/document";
+// import { ServerStyleSheet } from "styled-components";
 
-export default class MyDocument extends Document {
+// export default class MyDocument extends Document {
+//   static async getInitialProps(ctx: DocumentContext) {
+//     const sheet = new ServerStyleSheet();
+//     const originalRenderPage = ctx.renderPage;
+
+//     try {
+//       ctx.renderPage = () =>
+//         originalRenderPage({
+//           enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+//         });
+
+//       const initialProps = await Document.getInitialProps(ctx);
+//       return {
+//         ...initialProps,
+//         styles: (
+//           <>
+//             {initialProps.styles}
+//             {sheet.getStyleElement()}
+//           </>
+//         ),
+//       };
+//     } finally {
+//       sheet.seal();
+//     }
+//   }
+// }
+import {
+  createDOMRenderer,
+  renderToStyleElements,
+} from "@fluentui/react-components";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
+    // 👇 creates a renderer that will be used for SSR
+    const renderer = createDOMRenderer();
     const originalRenderPage = ctx.renderPage;
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-        });
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) =>
+          function EnhancedApp(props) {
+            const enhancedProps = {
+              ...props,
+              // 👇 this is required to provide a proper renderer instance
+              renderer,
+            };
 
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
+            return <App {...enhancedProps} />;
+          },
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = renderToStyleElements(renderer);
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {/* 👇 adding Fluent UI styles elements to output */}
+          {styles}
+        </>
+      ),
+    };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
+
+export default MyDocument;
